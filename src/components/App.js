19bb74e9b52ad9ae20ce43b0f8efee1b5e5029ab
Aniket240700 <104688183+Aniket240700@users.noreply.github.com>
2022-09-30 17:08:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+// checking 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Portfolio Site</h1>
+      <h3 data-ns-test="project-description">Hi I am Md Amir Sohail and this is the my portfolio website built using react library</h3>
+    </div>
   )
 }
 
